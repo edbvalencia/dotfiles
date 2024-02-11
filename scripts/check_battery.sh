c7f7daf75c258204bcf6f7ac22607ff24154de1d
Eduardo Valencia <127812245+edbvalencia@users.noreply.github.com>
2024-02-11 09:51:41 -0500

    Update check_battery.sh

--- scripts/check_battery.sh
+++ scripts/check_battery.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-#Add this line to crontab "crontab -e"
-#*/5 * * * * cd ~/.config/scripts/ && ./check_battery.sh
+# Añade esta línea a tu crontab: "crontab -e"
+# */5 * * * * cd ~/.config/scripts/ && ./check_battery.sh
 
 MIN_BATTERY_LEVEL=30
-CURRENT_BATTERY_LEVEL=$(acpi -b | grep -P -o '[0-9]+(?=%)')
 
-if [ "$CURRENT_BATTERY_LEVEL" -le "$MIN_BATTERY_LEVEL" ]; then
-    export DISPLAY=:0 #exporta al entorno grafico adecuado
+# Obtiene el nivel actual de la batería y el estado de carga
+BATTERY_INFO=$(acpi -b)
+CURRENT_BATTERY_LEVEL=$(echo "$BATTERY_INFO" | grep -Po '[0-9]+(?=%)')
+CHARGING=$(echo "$BATTERY_INFO" | grep -c "Charging")
+
+# Verifica si el nivel de batería es menor o igual al mínimo y si la batería no está cargando
+if [ "$CURRENT_BATTERY_LEVEL" -le "$MIN_BATTERY_LEVEL" ] && [ "$CHARGING" -eq 0 ]; then
+    export DISPLAY=:0 
     notify-send -u critical -t 5000 "Battery Low $CURRENT_BATTERY_LEVEL%. Please connect to power."
 fi
